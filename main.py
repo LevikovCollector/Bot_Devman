f0bbb270f8b2c7c47ff0acde2795fadaaf19e964
VladimirL <v.v.levikov@gmail.com>
2021-01-08 21:06:47 +0300

    Исправлено наименование переснной; исправлено формирования сообщения.

--- main.py
+++ main.py
@@ -11,31 +11,30 @@ if __name__ == '__main__':
     load_dotenv(dotenv_path='.env')
     header = {'Authorization': f'Token {os.getenv("DEVMAN_TOKEN")}'}
     bot = telegram.Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
-    timestamp = ''
+    timestamp = None
     while True:
         try:
             request_param = {'timestamp': timestamp}
             response = requests.get(f'{DEVMAN_API}long_polling/', params=request_param, headers=header, timeout=95)
             response.raise_for_status()
-            response_json = response.json()
-            status = response_json['status']
+            response_data = response.json()
+            status = response_data['status']
             if status == 'timeout':
-                timestamp = response_json['timestamp_to_request']
+                timestamp = response_data['timestamp_to_request']
             else:
-                message_text = ''
-                timestamp = response_json['last_attempt_timestamp']
-                for attempts in response_json['new_attempts']:
+                timestamp = response_data['last_attempt_timestamp']
+                for attempts in response_data:
                     work_status = attempts['is_negative']
                     title = attempts['lesson_title']
                     url = f"https://dvmn.org{attempts['lesson_url']}"
                     if work_status:
-                        message_text = f'У вас проверили работу «{title}»' \
-                                       f'\nК сожалению нашлись ошибки.' \
-                                       f'\nСсылка на урок: {url}'
+                        message_text = f'''У вас проверили работу «{title}»
+                                       \nК сожалению нашлись ошибки.
+                                       \nСсылка на урок: {url}'''
                     else:
-                        message_text = f'У вас проверили работу «{title}»' \
-                                       f'\nПреподавателю все понравилось, можно приступать к следующему уроку!' \
-                                       f'\nСсылка на урок: {url}'
+                        message_text = f'''У вас проверили работу «{title}»
+                                       \nПреподавателю все понравилось, можно приступать к следующему уроку!
+                                       \nСсылка на урок: {url}'''
 
                     bot.send_message(chat_id=os.getenv("TELEGRAM_CHAT_ID"), text=message_text)
         except ReadTimeout:
