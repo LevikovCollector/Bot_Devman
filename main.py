222e885850eab5ad1971172d8ec06a14b92d8944
VladimirL <v.v.levikov@gmail.com>
2021-01-09 17:30:10 +0300

    Добвлено логирование; подключен логгер бот

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ import os
 from dotenv import load_dotenv
 import telegram
 import time
+import logging
+from bot_logging import BotLogsHandler
 
 DEVMAN_API='https://dvmn.org/api/'
 
 if __name__ == '__main__':
     load_dotenv(dotenv_path='.env')
+    logging.basicConfig(format="%(levelname)s %(message)s")
+    bot_logger = logging.getLogger("Бот логер")
+    bot_logger.setLevel(logging.WARNING)
+    bot_logger.addHandler(BotLogsHandler())
+
     header = {'Authorization': f'Token {os.environ("DEVMAN_TOKEN")}'}
     bot = telegram.Bot(token=os.environ('TELEGRAM_BOT_TOKEN'))
+    chat_id = os.environ("TELEGRAM_CHAT_ID")
     timestamp = None
+    bot_logger.info('Бот запущен!')
+
     while True:
         try:
             request_param = {'timestamp': timestamp}
@@ -36,10 +46,16 @@ if __name__ == '__main__':
                                        \nПреподавателю все понравилось, можно приступать к следующему уроку!
                                        \nСсылка на урок: {url}'''
 
-                    bot.send_message(chat_id=os.environ("TELEGRAM_CHAT_ID"), text=message_text)
+                    bot.send_message(chat_id=chat_id, text=message_text)
         except ReadTimeout:
             pass
-        except ConnectionError:
+        except ConnectionError as error:
+            bot_logger.error(f'В работе бота возникла ошибка:\n{error}',exc_info=True)
             time.sleep(60)
-        except HTTPError:
-            bot.send_message(chat_id=os.getenv("TELEGRAM_CHAT_ID"), text='Ошибка сервера!\nСервер времено не доступен!')
+        except HTTPError as error:
+            bot_logger.error(f'В работе бота возникла ошибка:\n{error}',exc_info=True)
+            bot.send_message(chat_id=chat_id, text='Ошибка сервера!\nСервер времено не доступен!')
+            time.sleep(60)
+        except Exception as error:
+            bot_logger.error(f'В работе бота возникла ошибка:\n{error}',exc_info=True)
+            time.sleep(60)
